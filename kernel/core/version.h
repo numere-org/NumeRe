acdb2231d23f6a1174d59f2e6fd91eb2b79d2e24
numeredev <dev@numere.org>
2018-05-27 08:22:44 +0000

    Updated versioning file for new release candidate
    git-svn-id: https://svn.code.sf.net/p/numere/source/trunk@344 d50cd26f-24d5-421e-9e7d-30b8b4216d91

--- kernel/core/version.h
+++ kernel/core/version.h
@@ -4,26 +4,26 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "04";
+	static const char DATE[] = "27";
 	static const char MONTH[] = "05";
 	static const char YEAR[] = "2018";
 	static const char UBUNTU_VERSION_STYLE[] =  "18.05";
 	
 	//Software Status
-	static const char STATUS[] =  "Release Candidate 1";
-	static const char STATUS_SHORT[] =  "rc1";
+	static const char STATUS[] =  "Release Candidate 2";
+	static const char STATUS_SHORT[] =  "rc2";
 	
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 1;
 	static const long BUILD  = 1;
-	static const long REVISION  = 293;
+	static const long REVISION  = 305;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 3840;
-	#define RC_FILEVERSION 1,1,1,293
-	#define RC_FILEVERSION_STRING "1, 1, 1, 293\0"
-	static const char FULLVERSION_STRING [] = "1.1.1.293";
+	static const long BUILDS_COUNT  = 3857;
+	#define RC_FILEVERSION 1,1,1,305
+	#define RC_FILEVERSION_STRING "1, 1, 1, 305\0"
+	static const char FULLVERSION_STRING [] = "1.1.1.305";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
