11ecb50d7bbb7ada555c6180acdb28403d179cc5
numeredev <dev@numere.org>
2020-05-11 15:18:50 +0000

    Final changes for version 1.1.2rc3
    git-svn-id: https://svn.code.sf.net/p/numere/source/trunk@714 d50cd26f-24d5-421e-9e7d-30b8b4216d91

--- kernel/core/version.h
+++ kernel/core/version.h
@@ -4,26 +4,26 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "17";
-	static const char MONTH[] = "12";
-	static const char YEAR[] = "2019";
-	static const char UBUNTU_VERSION_STYLE[] =  "19.12";
+	static const char DATE[] = "11";
+	static const char MONTH[] = "05";
+	static const char YEAR[] = "2020";
+	static const char UBUNTU_VERSION_STYLE[] =  "20.05";
 	
 	//Software Status
-	static const char STATUS[] =  "Release Candidate 2";
-	static const char STATUS_SHORT[] =  "rc2";
+	static const char STATUS[] =  "Release Candidate 3";
+	static const char STATUS_SHORT[] =  "rc3";
 	
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 1;
 	static const long BUILD  = 2;
-	static const long REVISION  = 639;
+	static const long REVISION  = 658;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 4198;
-	#define RC_FILEVERSION 1,1,2,639
-	#define RC_FILEVERSION_STRING "1, 1, 2, 639\0"
-	static const char FULLVERSION_STRING [] = "1.1.2.639";
+	static const long BUILDS_COUNT  = 4201;
+	#define RC_FILEVERSION 1,1,2,658
+	#define RC_FILEVERSION_STRING "1, 1, 2, 658\0"
+	static const char FULLVERSION_STRING [] = "1.1.2.658";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
