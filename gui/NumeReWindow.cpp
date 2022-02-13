5f172a540700b76da9c02ef74d07c8a03efe0ef2
numeredev <dev@numere.org>
2022-02-13 15:31:54 +0000

    Implemented ticket #656 Added log information to the GUI exception handler
    git-svn-id: https://svn.code.sf.net/p/numere/source/trunk@1071 d50cd26f-24d5-421e-9e7d-30b8b4216d91

--- gui/NumeReWindow.cpp
+++ gui/NumeReWindow.cpp
@@ -358,10 +358,12 @@ bool MyApp::OnExceptionInMainLoop()
     }
     catch (std::exception& e)
     {
+        g_logger.error(std::string("Unexpected exception: ") + e.what());
         wxMessageBox(std::string("An unexpected exception was caught. If it is reproducable, consider informing us about this issue. Message: ") + e.what(), "Exception caught");
     }
     catch (SyntaxError& e)
     {
+        g_logger.error("Unexpected exception: " + toString((int)e.errorcode) + ", message: " + _guilang.get("ERR_NR_" + toString((int)e.errorcode) + "_0_*", e.getToken(), toString(e.getIndices()[0]), toString(e.getIndices()[1]), toString(e.getIndices()[2]), toString(e.getIndices()[3])) + ", expression: " + e.getExpr() + ", token: " + e.getToken());
         wxMessageBox("An unexpected exception was caught. If it is reproducable, consider informing us about this issue. Code: " + toString((int)e.errorcode) + ", message: " + _guilang.get("ERR_NR_" + toString((int)e.errorcode) + "_0_*", e.getToken(), toString(e.getIndices()[0]), toString(e.getIndices()[1]), toString(e.getIndices()[2]), toString(e.getIndices()[3])) + ", expression: " + e.getExpr() + ", token: " + e.getToken(), "Exception caught");
     }
     catch ( ... )
