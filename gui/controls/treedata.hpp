bd537a2344e2df43f0137678cdb984c38484d0db
numeredev <dev@numere.org>
2022-07-25 14:25:03 +0000

    Implemented ticket #530 "Allow searching for keywords in the documentation browser"
    git-svn-id: https://svn.code.sf.net/p/numere/source/trunk@1194 d50cd26f-24d5-421e-9e7d-30b8b4216d91

---
+++ gui/controls/treedata.hpp
@@ -0,0 +1,50 @@
+/*****************************************************************************
+    NumeRe: Framework fuer Numerische Rechnungen
+    Copyright (C) 2022  Erik Haenel et al.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+
+#include "wx/treectrl.h"
+
+/////////////////////////////////////////////////
+/// \brief This class provides the basic
+/// functionality to provide a tooltip for a tree
+/// item.
+/////////////////////////////////////////////////
+class ToolTipTreeData : public wxTreeItemData
+{
+    public:
+        ToolTipTreeData(const wxString& tip = wxEmptyString) : tooltip(tip) {};
+
+        wxString tooltip;
+};
+
+
+/////////////////////////////////////////////////
+/// \brief This class provides the needed
+/// functionalities for the file tree and the
+/// symbols tree.
+/////////////////////////////////////////////////
+class FileNameTreeData : public ToolTipTreeData
+{
+    public:
+        FileNameTreeData() : ToolTipTreeData(), isDir(false), isFunction(false), isCommand(false), isConstant(false) {};
+        bool isDir;
+        bool isFunction;
+        bool isCommand;
+        bool isConstant;
+        wxString filename;
+};
