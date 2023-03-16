5196e3860bc36b85addf73f28ea5a18670357370
numeredev <dev@numere.org>
2023-03-16 09:58:11 +0100

    Bumped version number for release

--- kernel/core/version.h
+++ kernel/core/version.h
@@ -4,30 +4,26 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char DATE[] = "19";
-	static const char MONTH[] = "10";
-	static const char YEAR[] = "2022";
-	static const char UBUNTU_VERSION_STYLE[] =  "22.10";
+	static const char DATE[] = "16";
+	static const char MONTH[] = "03";
+	static const char YEAR[] = "2023";
+	static const char UBUNTU_VERSION_STYLE[] =  "23.03";
 
 	//Software Status
-	static const char STATUS[] =  "Compton";
-	static const char STATUS_SHORT[] =  "";
+	static const char STATUS[] =  "Release Candidate";
+	static const char STATUS_SHORT[] =  "rc";
 
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 1;
-	static const long BUILD  = 4;
-	static const long REVISION  = 697;
+	static const long BUILD  = 5;
+	static const long REVISION  = 700;
 
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 4971;
-	#define RC_FILEVERSION 1,1,4,697
-	#define RC_FILEVERSION_STRING "1, 1, 4, 697\0"
-	static const char FULLVERSION_STRING [] = "1.1.4.697";
-
-	//SVN Version
-	static const char SVN_REVISION[] = "1240";
-	static const char SVN_DATE[] = "2022-10-15T18:21:14.295587Z";
+	static const long BUILDS_COUNT  = 4983;
+	#define RC_FILEVERSION 1,1,5,700
+	#define RC_FILEVERSION_STRING "1, 1, 5, 700\0"
+	static const char FULLVERSION_STRING [] = "1.1.5.700";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
