aeaff3ae1efc78736f3fbffffdfdb875aa770487
numeredev <dev@numere.org>
2023-08-30 22:27:46 +0200

    Added a check for validating indices

--- kernel/core/datamanagement/indices.cpp
+++ kernel/core/datamanagement/indices.cpp
@@ -183,6 +183,11 @@ void getIndices(StringView sCmd, Indices& _idx,  Parser& _parser, MemoryManager&
 
     // If the argument contains a comma, handle it as a usual index list
     handleArgumentForIndices(_idx, _parser, _data, _idx.sCompiledAccessEquation, sCmd);
+
+    // Check indices here
+    if (!_idx.row.checkRange() || !_idx.col.checkRange())
+        throw SyntaxError(SyntaxError::INVALID_INDEX, sCmd.to_string(), nPos+1,
+                          _idx.row.to_string() + ", " + _idx.col.to_string());
 }
 
 
