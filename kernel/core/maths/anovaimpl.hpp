084a408127511d13dc6613a34295fdb6456cf780
Erik Hänel <87755490+numeredev@users.noreply.github.com>
2024-01-24 19:01:04 +0100

    Apply suggestions from code review

--- kernel/core/maths/anovaimpl.hpp
+++ kernel/core/maths/anovaimpl.hpp
@@ -1,6 +1,6 @@
 /*****************************************************************************
     NumeRe: Framework fuer Numerische Rechnungen
-    Copyright (C) 2018  Erik Haenel et al.
+    Copyright (C) 2024 Erik Haenel et al.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
