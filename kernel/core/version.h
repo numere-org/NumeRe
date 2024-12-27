fd10085056d840ddccd289c94d3af065abb3fd7d
numeredev <dev@numere.org>
2024-12-28 00:22:01 +0100

    Updated version file for new beta release

--- kernel/core/version.h
+++ kernel/core/version.h
@@ -4,26 +4,26 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char DATE[] = "18";
-	static const char MONTH[] = "04";
+	static const char DATE[] = "28";
+	static const char MONTH[] = "12";
 	static const char YEAR[] = "2024";
-	static const char UBUNTU_VERSION_STYLE[] =  "24.04";
+	static const char UBUNTU_VERSION_STYLE[] =  "24.12";
 
 	//Software Status
-	static const char STATUS[] =  "Casimir";
-	static const char STATUS_SHORT[] =  "";
+	static const char STATUS[] =  "Beta";
+	static const char STATUS_SHORT[] =  "b";
 
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 1;
-	static const long BUILD  = 6;
+	static const long BUILD  = 7;
 	static const long REVISION  = 712;
 
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 5150;
-	#define RC_FILEVERSION 1,1,6,712
-	#define RC_FILEVERSION_STRING "1, 1, 6, 712\0"
-	static const char FULLVERSION_STRING [] = "1.1.6.712";
+	static const long BUILDS_COUNT  = 5152;
+	#define RC_FILEVERSION 1,1,7,712
+	#define RC_FILEVERSION_STRING "1, 1, 7, 712\0"
+	static const char FULLVERSION_STRING [] = "1.1.7.712";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
